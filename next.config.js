2fca3a0c2f9d4e37ea8723aa5c9d650bfbce4e51
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-08-14 11:54:00 +0300

    feature: sentry integration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config');
 const withImages = require('next-images');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-const nextConfig = withImages({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
   env: {
     ALCHEMY_APIKEY: process.env.ALCHEMY_APIKEY
   }
-});
+};
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(withImages(nextConfig),);
