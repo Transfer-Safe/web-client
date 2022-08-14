c78d6b1390861cadda499323f3d1b0e3a3d33dc7
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-08-14 12:29:48 +0300

    chore: refactor transfer buttons

--- components/InvoiceTransferView/InvoiceTransferView.tsx
+++ components/InvoiceTransferView/InvoiceTransferView.tsx
@@ -1,19 +1,18 @@
 import { Box, Container, Typography, useTheme } from '@mui/material';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Head from 'next/head';
 import Link from 'next/link';
-import React, { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { useAccount } from 'wagmi';
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { InvoiceTransferButtons } from './InvoiceTransferButtons';
 import style from './InvoiceTransferView.module.scss';
 
 import { Invoice } from '../../models';
 import { RootState } from '../../store/rootReducer';
+import { resetTransferInvoice } from '../../store/transferInvoice/actions';
 import { TransferInvoiceStatus } from '../../store/transferInvoice/types';
 import { formatNumber, formatTransactionId } from '../../utils';
 import AppModal from '../AppModal';
-import BuyCurrencyButton from '../BuyCurrencyButton';
 import FormattedNumber from '../FormattedNumber';
 import ThrobberSection from '../Throbber/ThrobberSection';
 export interface InvoiceTransferViewProps {
@@ -24,7 +23,7 @@ export const InvoiceTransferView: React.FC<InvoiceTransferViewProps> = ({
   invoice,
 }) => {
   const theme = useTheme();
-  const { isConnected } = useAccount();
+  const dispatch = useDispatch();
 
   const title = useMemo(() => {
     let title = formatNumber(invoice.amount) + '$ transfer request';
@@ -55,6 +54,19 @@ export const InvoiceTransferView: React.FC<InvoiceTransferViewProps> = ({
     return false;
   }, [transferInvoiceStatus, invoice.deposited]);
 
+  const showSuccesfullyTransferedModal = useMemo(() => {
+    if (transferInvoiceStatus === TransferInvoiceStatus.SUCCESS) {
+      if (invoice.deposited) {
+        return true;
+      }
+    }
+    return false;
+  }, [transferInvoiceStatus, invoice.deposited]);
+
+  useEffect(() => {
+    console.log('===> invoice.deposited', invoice.deposited);
+  }, [invoice.deposited]);
+
   return (
     <Box
       flex={1}
@@ -79,6 +91,15 @@ export const InvoiceTransferView: React.FC<InvoiceTransferViewProps> = ({
           />
         )}
       </AppModal>
+      <AppModal
+        open={showSuccesfullyTransferedModal}
+        title="Succesfully transfered"
+        onClose={() => dispatch(resetTransferInvoice())}
+      >
+        <Typography>
+          You have succesfully transfered {formatNumber(invoice.amount)}$
+        </Typography>
+      </AppModal>
       <Head>
         <title>{title}</title>
       </Head>
@@ -91,7 +112,6 @@ export const InvoiceTransferView: React.FC<InvoiceTransferViewProps> = ({
             wordBreak: 'break-word',
           }}
         >
-          {invoice.deposited && <span>Deposited! </span>}
           <Typography
             component="span"
             variant="h1"
@@ -115,63 +135,7 @@ export const InvoiceTransferView: React.FC<InvoiceTransferViewProps> = ({
             {formatTransactionId(invoice.receipientAddress)}
           </Typography>
         </Link>
-        <Box mt={2} display="flex" flexDirection="column" alignItems="center">
-          {!isConnected && <ConnectButton />}
-          {isConnected && (
-            <React.Fragment>
-              {invoice.isNativeToken && (
-                <BuyCurrencyButton
-                  invoiceId={invoice.id}
-                  variant="outlined"
-                  isNativeCurrency
-                  value={10}
-                  size="large"
-                  sx={{
-                    width: {
-                      xs: '100%',
-                      sm: 'inherit',
-                    },
-                    marginTop: 2,
-                  }}
-                />
-              )}
-              {invoice.availableTokenTypes.map((token) => (
-                <BuyCurrencyButton
-                  invoiceId={invoice.id}
-                  key={token}
-                  variant="outlined"
-                  tokenAddress={token}
-                  value={10}
-                  size="large"
-                  sx={{
-                    width: {
-                      xs: '100%',
-                      sm: 'inherit',
-                    },
-                    marginTop: 2,
-                  }}
-                />
-              ))}
-            </React.Fragment>
-          )}
-        </Box>
-        <Typography
-          color={theme.palette.grey[800]}
-          my={4}
-          textAlign="center"
-          variant="body2"
-        >
-          We will hold your transfer until you confirm it by pressing the
-          “Confirm” button. If you change your mind you can refund your deposit
-          after{' '}
-          <Box
-            component="span"
-            fontWeight="600"
-            color={theme.palette.primary.main}
-          >
-            2 weeks
-          </Box>
-        </Typography>
+        {!invoice.deposited && <InvoiceTransferButtons invoice={invoice} />}
       </Container>
     </Box>
   );
