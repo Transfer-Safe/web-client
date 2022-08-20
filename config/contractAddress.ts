a641b0fed2e1452a088056635d622a5873fa799c
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-08-20 18:46:51 +0300

    chore: update contract address

--- config/contractAddress.ts
+++ config/contractAddress.ts
@@ -3,7 +3,7 @@ import { chain } from 'wagmi';
 export const getRouterContractAddress = (chainId: number): string => {
   switch (chainId) {
     case chain.polygonMumbai.id:
-      return '0x394D884e3a2Dc5b526976aad85077C0A144e21f1';
+      return '0x165c02907c40F2f8618503b43cCB6449DbFa2FAE';
     default:
       throw new Error(`Unknown chainId: ${chainId}`);
   }
