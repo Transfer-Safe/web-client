2f2ca49cb296271fd07c2d651858ce03738b9481
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-08-20 18:09:10 +0300

    chore: add encryption logging

--- utils/encryptEmail.ts
+++ utils/encryptEmail.ts
@@ -2,11 +2,23 @@ import CryptoJS from 'crypto-js';
 
 const secret = process.env.EMAIL_SECRET || '';
 
-export const encryptEmail = (email: string): string =>
-  CryptoJS.AES.encrypt(email, secret).toString();
+export const encryptEmail = (email: string): string => {
+  console.log('Encrypt email. Secret: ', secret);
+  console.log('Encrypt email. Email: ', email);
+  const result = CryptoJS.AES.encrypt(email, secret).toString();
+  console.log('Encrypt email. Encrypted email: ', result);
+  return result;
+};
 
-export const dencryptEmail = (email: string): string =>
-  CryptoJS.AES.decrypt(email, secret).toString(CryptoJS.enc.Utf8);
+export const dencryptEmail = (email: string): string => {
+  console.log('Decrypt email. Secret: ', secret);
+  console.log('Decrypt email. Email: ', email);
+  const result = CryptoJS.AES.decrypt(email, secret).toString(
+    CryptoJS.enc.Utf8,
+  );
+  console.log('Decrypt email. Decrypted email: ', result);
+  return result;
+};
 
 export const encryptEmailClient = async (email: string): Promise<string> => {
   const res = await fetch('/api/encryptEmail', {
