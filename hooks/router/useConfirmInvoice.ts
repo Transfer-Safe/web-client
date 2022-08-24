77e1e9aa23f6033fd00fba329ef8e4a36f503b7b
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-08-24 16:05:21 +0300

    hotfix: UNPREDICTABLE_GAS_LIMIT issues

--- hooks/router/useConfirmInvoice.ts
+++ hooks/router/useConfirmInvoice.ts
@@ -1,8 +1,21 @@
+import { BigNumber } from 'ethers';
+import { useState } from 'react';
 import { useContractWrite } from 'wagmi';
 
 import { useWriteRouterFunction } from './useRouterFunction';
 
 export const useConfirmInvoice = (invoiceId: string) => {
-  const { config } = useWriteRouterFunction('confirmInvoice', [invoiceId]);
+  const [gasLimit, setGasLimit] = useState<BigNumber | undefined>();
+  const { config } = useWriteRouterFunction('confirmInvoice', [invoiceId], {
+    overrides: {
+      gasLimit,
+    },
+    onError(err) {
+      if (err.message.includes('UNPREDICTABLE_GAS_LIMIT')) {
+        setGasLimit(BigNumber.from('0x01c385').mul(3));
+      }
+    },
+  });
+
   return useContractWrite(config);
 };
