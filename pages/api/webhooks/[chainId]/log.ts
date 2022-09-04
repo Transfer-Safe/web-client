6d6a0537e16a77366a501957cd47942004d90d43
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-09-04 16:04:51 +0300

    chore: add log logging

--- pages/api/webhooks/[chainId]/log.ts
+++ pages/api/webhooks/[chainId]/log.ts
@@ -1,5 +1,6 @@
 import { TransferSafeRouter__factory } from '@transfer-safe/router';
 import { BigNumber } from 'ethers';
+import { LogDescription } from 'ethers/lib/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { handleLogEvent } from '../../../../utils/webhooks';
@@ -10,10 +11,16 @@ export default async function handler(
 ) {
   const chainId = BigNumber.from(req.query.chainId).toNumber();
   const router = TransferSafeRouter__factory.createInterface();
-  const log = router.parseLog(req.body);
+  let log: LogDescription | undefined;
+  try {
+    log = router.parseLog(req.body);
+  } catch (err) {
+    console.error('Failed to parse log', err);
+    res.status(500).send(undefined);
+  }
   const txId = req.body.transaction_hash;
   try {
-    await handleLogEvent(log, chainId, txId);
+    await handleLogEvent(log!, chainId, txId);
     res.status(200).send(undefined);
   } catch (err) {
     res.status(500).send(undefined);
